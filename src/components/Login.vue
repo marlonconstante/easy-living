702d9228ccdfe2655c2ad56f40cd6fe094101195
Marlon Santos Constante <marlon.constante@gmail.com>
2017-10-08 19:40:22 -0300

    Componente Slogan

--- src/components/Login.vue
+++ src/components/Login.vue
@@ -2,15 +2,19 @@
     <div class="content">
         <div class="box">
             <img src="../assets/logo.svg">
-            <label class="slogan">Everything you need. We deliver.</label>
+            <slogan />
             <form v-on:submit.prevent="signIn">
                 <input type="email" v-model="user.email" placeholder="E-MAIL">
                 <input type="password" v-model="user.password" placeholder="PASSWORD">
                 <md-button class="md-fab">
                     <md-icon class="md-size-2x">arrow_forward</md-icon>
                 </md-button>
-                <a>YOU DO NOT HAVE AN ACCOUNT?</a>
-                <a>FORGOT PASSWORD?</a>
+                <div class="link">
+                    <md-ink-ripple /> YOU DO NOT HAVE AN ACCOUNT?
+                </div>
+                <div class="link">
+                    <md-ink-ripple /> FORGOT PASSWORD?
+                </div>
             </form>
         </div>
     </div>
@@ -18,8 +22,10 @@
 
 <script>
 import { auth } from '../services/firebase'
+import Slogan from '@/components/Slogan'
 
 export default {
+    name: 'Login',
     data() {
         return {
             user: {
@@ -27,6 +33,9 @@ export default {
                 password: ''
             }
         }
+    },
+    components: {
+        Slogan
     },
     methods: {
         register: function() {
@@ -66,12 +75,6 @@ export default {
     width: 400px;
 }
 
-label.slogan {
-    display: block;
-    font-size: 22.6px;
-    margin: 21.5px 0 60px 0;
-}
-
 input {
     display: block;
     width: 100%;
@@ -87,6 +90,7 @@ input {
 
 input:focus {
     outline: none;
+    box-shadow: 0 0 30px #8a4447;
 }
 
 input::-webkit-input-placeholder {
@@ -105,12 +109,12 @@ input:-ms-input-placeholder {
     color: #fc4349;
 }
 
-.md-button {
+.md-button.md-fab {
     margin: 30px;
     width: 91px;
     height: 91px;
     border: 1px solid rgba(0, 0, 0, 0.4);
-    box-shadow: inset 0 0 1px rgba(255, 255, 255, 0.8), 0 8px 0 rgba(0, 0, 0, 0.1) !important;
+    box-shadow: inset 0 0 1px rgba(255, 255, 255, 0.8), 0 8px 0 rgba(0, 0, 0, 0.1);
     background-color: #2c3e50 !important;
 }
 
@@ -118,13 +122,12 @@ input:-ms-input-placeholder {
     color: #cb434d;
 }
 
-a {
-    display: block;
+.link {
     cursor: pointer;
-    margin: 30px;
+    position: relative;
+    padding: 15px;
     font-size: 14px;
     font-weight: 700;
-    text-decoration: none !important;
-    color: #ffffff !important;
+    color: #ffffff;
 }
 </style>
---
+++ src/components/Slogan.vue
@@ -0,0 +1,24 @@
+<template>
+    <div class="slogan">
+        <label>{{ message }}</label>
+    </div>
+</template>
+
+<script>
+export default {
+    name: 'Slogan',
+    data() {
+        return {
+            message: 'Everything you need. We deliver.'
+        }
+    }
+}
+</script>
+
+<style scoped>
+.slogan label {
+    display: block;
+    font-size: 22.6px;
+    margin: 21.5px 0 60px 0;
+}
+</style>
