3a7921ee8bb717050967627aaf9c0e796ddbdca0
Marlon Santos Constante <marlon.constante@gmail.com>
2017-10-09 15:20:09 -0300

    Componente UserBox

---
+++ src/components/UserBox.vue
@@ -0,0 +1,57 @@
+<template>
+    <div class="userBox">
+        <div class="info">
+            <label class="name">{{ name }}</label>
+            <md-icon>account_circle</md-icon>
+        </div>
+        <div class="community">{{ community }}</div>
+        <div class="address">{{ address }}</div>
+    </div>
+</template>
+
+<script>
+export default {
+    name: 'UserBox',
+    props: {
+        name: {
+            type: String
+        },
+        community: {
+            type: String
+        },
+        address: {
+            type: String
+        }
+    }
+}
+</script>
+
+<style scoped>
+.userBox {
+    width: 243px;
+    font-size: 16px;
+    text-align: right;
+    color: #ffffff;
+}
+
+.userBox .info {
+    display: inline-flex;
+}
+
+.userBox .info .name {
+    font-weight: 700;
+    text-transform: uppercase;
+    margin: 3px 10px 0 0;
+}
+
+.userBox .community {
+    opacity: 0.8;
+    font-weight: 500;
+    font-style: italic;
+    margin: 13px 0 10px 0;
+}
+
+.userBox .address {
+    opacity: 0.8;
+}
+</style>
