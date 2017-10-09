970089ef43dccf2c4023ef378217fd3aa0d2b6c0
Marlon Santos Constante <marlon.constante@gmail.com>
2017-10-08 22:53:30 -0300

    Componente RoundButton

---
+++ src/components/RoundButton.vue
@@ -0,0 +1,42 @@
+<template>
+    <div class="roundButton">
+        <md-button class="md-fab" @click="onClick">
+            <md-icon class="md-size-2x">{{ iconClass }}</md-icon>
+            <slot />
+        </md-button>
+    </div>
+</template>
+
+<script>
+export default {
+    name: 'RoundButton',
+    props: {
+        iconClass: {
+            type: String
+        }
+    },
+    methods: {
+        onClick: function(event) {
+            this.$emit('click', event)
+        }
+    }
+}
+</script>
+
+<style scoped>
+.roundButton .md-button.md-fab {
+    margin: 30px;
+    width: 91px;
+    height: 91px;
+    border: 1px solid rgba(0, 0, 0, 0.4);
+    box-shadow: inset 0 0 1px rgba(255, 255, 255, 0.8), 0 8px 0 rgba(0, 0, 0, 0.1);
+    font-size: 24px;
+    font-weight: 700;
+    color: #fc4349;
+    background-color: #2c3e50;
+}
+
+.roundButton .md-button.md-fab:hover {
+    background-color: #2c3e50;
+}
+</style>
