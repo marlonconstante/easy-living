a4122751d3e940ec23b63bbb2c48c983ff21f7f9
Marlon Santos Constante <marlon.constante@gmail.com>
2017-10-08 22:47:37 -0300

    Componente LinkButton

---
+++ src/components/LinkButton.vue
@@ -0,0 +1,28 @@
+<template>
+    <div class="linkButton" @click="onClick">
+        <md-ink-ripple />
+        <slot />
+    </div>
+</template>
+
+<script>
+export default {
+    name: 'LinkButton',
+    methods: {
+        onClick: function(event) {
+            this.$emit('click', event)
+        }
+    }
+}
+</script>
+
+<style scoped>
+.linkButton {
+    cursor: pointer;
+    position: relative;
+    padding: 15px;
+    font-size: 14px;
+    font-weight: 700;
+    color: #ffffff;
+}
+</style>
