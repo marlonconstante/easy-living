8fc6b1befe102352297a0969e15d9c90361bb718
Marlon Santos Constante <marlon.constante@gmail.com>
2017-10-08 21:48:50 -0300

    Componente InputText

---
+++ src/components/InputText.vue
@@ -0,0 +1,55 @@
+<template>
+    <div class="inputText">
+        <input :type="type" :value="value" :placeholder="placeholder" @input="updateValue($event.target.value)">
+    </div>
+</template>
+
+<script>
+export default {
+    name: 'InputText',
+    props: {
+        type: {
+            type: String
+        },
+        value: {
+            type: String
+        },
+        placeholder: {
+            type: String
+        }
+    },
+    methods: {
+        updateValue: function(value) {
+            this.$emit('input', value)
+        }
+    }
+}
+</script>
+
+<style scoped>
+.inputText input {
+    display: block;
+    width: 100%;
+    height: 64px;
+    margin: 20px 0;
+    border: none;
+    border-radius: 6px;
+    font-family: 'Archivo';
+    font-size: 16px;
+    text-indent: 20px;
+    color: #fc4349;
+}
+
+.inputText input::placeholder {
+    color: #fc4349;
+}
+
+.inputText input::-moz-placeholder {
+    opacity: 1;
+}
+
+.inputText input:focus {
+    outline: none;
+    box-shadow: 0 0 30px #8a4447;
+}
+</style>
--- src/components/Login.vue
+++ src/components/Login.vue
@@ -3,19 +3,17 @@
         <div class="box">
             <img src="../assets/logo.svg">
             <slogan />
-            <form v-on:submit.prevent="signIn">
-                <input type="email" v-model="user.email" placeholder="E-MAIL">
-                <input type="password" v-model="user.password" placeholder="PASSWORD">
-                <md-button class="md-fab">
-                    <md-icon class="md-size-2x">arrow_forward</md-icon>
-                </md-button>
-                <div class="link">
-                    <md-ink-ripple /> YOU DO NOT HAVE AN ACCOUNT?
-                </div>
-                <div class="link">
-                    <md-ink-ripple /> FORGOT PASSWORD?
-                </div>
-            </form>
+            <input-text type="email" v-model="user.email" placeholder="E-MAIL" />
+            <input-text type="password" v-model="user.password" placeholder="PASSWORD" />
+            <md-button class="md-fab" @click="signIn">
+                <md-icon class="md-size-2x">arrow_forward</md-icon>
+            </md-button>
+            <div class="link">
+                <md-ink-ripple /> YOU DO NOT HAVE AN ACCOUNT?
+            </div>
+            <div class="link">
+                <md-ink-ripple /> FORGOT PASSWORD?
+            </div>
         </div>
     </div>
 </template>
@@ -23,6 +21,7 @@
 <script>
 import { auth } from '../services/firebase'
 import Slogan from '@/components/Slogan'
+import InputText from '@/components/InputText'
 
 export default {
     name: 'Login',
@@ -35,7 +34,8 @@ export default {
         }
     },
     components: {
-        Slogan
+        Slogan,
+        InputText
     },
     methods: {
         register: function() {
@@ -71,42 +71,8 @@ export default {
 }
 
 .box {
-    margin: auto;
+    margin: 60px auto;
     width: 400px;
-}
-
-input {
-    display: block;
-    width: 100%;
-    height: 64px;
-    margin: 20px 0;
-    border: none;
-    border-radius: 6px;
-    font-family: 'Archivo';
-    font-size: 16px;
-    text-indent: 20px;
-    color: #fc4349;
-}
-
-input:focus {
-    outline: none;
-    box-shadow: 0 0 30px #8a4447;
-}
-
-input::-webkit-input-placeholder {
-    color: #fc4349;
-}
-
-input:-moz-placeholder {
-    color: #fc4349;
-}
-
-input::-moz-placeholder {
-    color: #fc4349;
-}
-
-input:-ms-input-placeholder {
-    color: #fc4349;
 }
 
 .md-button.md-fab {
