ff0ac2b3850ee42e7515eff2236a5dceb51dcee0
Marlon Santos Constante <marlon.constante@gmail.com>
2017-10-11 09:48:33 -0300

    Componente InputTextArea

---
+++ src/components/InputTextArea.vue
@@ -0,0 +1,53 @@
+<template>
+    <div class="inputTextArea">
+        <textarea :value="value" :placeholder="placeholder" @input="onInput" />
+    </div>
+</template>
+
+<script>
+export default {
+    name: 'InputTextArea',
+    props: {
+        value: {
+            type: String
+        },
+        placeholder: {
+            type: String
+        }
+    },
+    methods: {
+        onInput: function(event) {
+            this.$emit('input', event.target.value)
+        }
+    }
+}
+</script>
+
+<style scoped>
+.inputTextArea textarea {
+    display: block;
+    resize: none;
+    width: 100%;
+    height: 128px;
+    margin: 20px 0;
+    padding: 20px;
+    border: none;
+    border-radius: 6px;
+    font-family: 'Archivo';
+    font-size: 16px;
+    color: #fc4349;
+}
+
+.inputTextArea textarea::placeholder {
+    color: #fc4349;
+}
+
+.inputTextArea textarea::-moz-placeholder {
+    opacity: 1;
+}
+
+.inputTextArea textarea:focus {
+    outline: none;
+    box-shadow: 0 0 30px #8a4447;
+}
+</style>
