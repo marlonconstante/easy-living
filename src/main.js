2a15830df40d16a7c7e031675ed4a0c2a560d31a
Marlon Santos Constante <marlon.constante@gmail.com>
2017-10-12 13:55:12 -0300

    Organizando configuração do VueMask

--- src/main.js
+++ src/main.js
@@ -7,20 +7,15 @@ import VueFire from 'vuefire'
 import VueMaterial from 'vue-material'
 import 'vue-material/dist/vue-material.css'
 
-import VueMask from 'v-mask'
-import { VueMaskDirective } from 'v-mask'
-
 import App from './App'
 import router from './router'
+import mask from './mask'
 
 Vue.config.productionTip = false
 
 Vue.use(Vuex)
 Vue.use(VueFire)
 Vue.use(VueMaterial)
-
-Vue.use(VueMask)
-Vue.directive('mask', VueMaskDirective)
 
 /* eslint-disable no-new */
 new Vue({
