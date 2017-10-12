f8bec1a71bc873f4bba0e496b772552e7f593b10
Marlon Santos Constante <marlon.constante@gmail.com>
2017-10-12 09:23:00 -0300

    Tela de cadastro do cliente

---
+++ src/components/Customer.vue
@@ -0,0 +1,61 @@
+<template>
+    <div class="content">
+        <div class="box">
+            <img src="../assets/logo.svg">
+            <slogan />
+            <input-text type="text" v-model="user.name" placeholder="NAME" />
+            <input-text type="date" v-model="user.birthDate" placeholder="DATE OF BIRTH" />
+            <input-text-area v-model="user.address" placeholder="ADDRESS" />
+            <input-text type="text" v-model="user.community" placeholder="COMMUNITY NAME" />
+            <round-button iconClass="arrow_forward" @click="save" />
+            <link-button>I ALREADY HAVE AN ACCOUNT</link-button>
+        </div>
+    </div>
+</template>
+
+<script>
+import Slogan from '@/components/Slogan'
+import InputText from '@/components/InputText'
+import InputTextArea from '@/components/InputTextArea'
+import RoundButton from '@/components/RoundButton'
+import LinkButton from '@/components/LinkButton'
+
+export default {
+    name: 'Login',
+    data() {
+        return {
+            user: {
+                name: '',
+                birthDate: '',
+                address: '',
+                community: ''
+            }
+        }
+    },
+    components: {
+        Slogan,
+        InputText,
+        InputTextArea,
+        RoundButton,
+        LinkButton
+    },
+    methods: {
+        save: function() {
+            const user = this.user
+            console.log(user)
+        }
+    }
+}
+</script>
+
+<style scoped>
+.content {
+    display: table-cell;
+    vertical-align: middle;
+}
+
+.box {
+    margin: 60px auto;
+    width: 400px;
+}
+</style>
--- src/router/index.js
+++ src/router/index.js
@@ -1,6 +1,7 @@
 import Vue from 'vue'
 import Router from 'vue-router'
 import Login from '@/components/Login'
+import Customer from '@/components/Customer'
 
 Vue.use(Router)
 
@@ -10,6 +11,11 @@ export default new Router({
             path: '/login',
             name: 'Login',
             component: Login
+        },
+        {
+            path: '/customer',
+            name: 'Customer',
+            component: Customer
         }
     ]
 })
