4eebfd322acc44d493aa5b84068e345739ea48b4
Marlon Santos Constante <marlon.constante@gmail.com>
2017-10-12 10:58:22 -0300

    Configuração do plugin v-mask

--- src/main.js
+++ src/main.js
@@ -2,15 +2,23 @@
 // (runtime-only or standalone) has been set in webpack.base.conf with an alias.
 import Vue from 'vue'
 import VueFire from 'vuefire'
+
 import VueMaterial from 'vue-material'
 import 'vue-material/dist/vue-material.css'
+
+import VueMask from 'v-mask'
+import { VueMaskDirective } from 'v-mask'
 
 import App from './App'
 import router from './router'
 
 Vue.config.productionTip = false
+
 Vue.use(VueFire)
 Vue.use(VueMaterial)
+
+Vue.use(VueMask)
+Vue.directive('mask', VueMaskDirective)
 
 /* eslint-disable no-new */
 new Vue({
