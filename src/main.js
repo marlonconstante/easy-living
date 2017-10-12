fb1bb4ce9a23e8cc5d5b41dea908075e538d056b
Marlon Santos Constante <marlon.constante@gmail.com>
2017-10-12 13:43:10 -0300

    vuex

--- src/main.js
+++ src/main.js
@@ -1,6 +1,7 @@
 // The Vue build version to load with the `import` command
 // (runtime-only or standalone) has been set in webpack.base.conf with an alias.
 import Vue from 'vue'
+import Vuex from 'vuex'
 import VueFire from 'vuefire'
 
 import VueMaterial from 'vue-material'
@@ -14,6 +15,7 @@ import router from './router'
 
 Vue.config.productionTip = false
 
+Vue.use(Vuex)
 Vue.use(VueFire)
 Vue.use(VueMaterial)
 
