df570eb6b4cb6fc3fe48352f417c98e2fccb86f9
Marlon Santos Constante <marlon.constante@gmail.com>
2017-10-15 05:22:03 -0200

    Firebase: Carregando dados do usuário logado

--- src/App.vue
+++ src/App.vue
@@ -5,8 +5,23 @@
 </template>
 
 <script>
+import Firebase from 'firebase'
+
 export default {
-    name: 'App'
+    name: 'App',
+    beforeCreate() {
+        Firebase.auth().onAuthStateChanged((user) => {
+            if (user) {
+                const customerRef = Firebase.database().ref(`customers/${user.uid}`)
+                this.$bindAsObject('user', customerRef, null, () => {
+                    this.user.id = user.uid
+                    this.user.email = user.email
+
+                    console.log(this.user)
+                })
+            }
+        });
+    }
 }
 </script>
 
