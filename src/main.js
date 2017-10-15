a073a4c926194c967e21c56da5d927614889a8e6
Marlon Santos Constante <marlon.constante@gmail.com>
2017-10-15 02:57:42 -0200

    Alterando ordem dos imports

--- src/main.js
+++ src/main.js
@@ -1,15 +1,16 @@
 // The Vue build version to load with the `import` command
 // (runtime-only or standalone) has been set in webpack.base.conf with an alias.
-import Vue from 'vue'
 
+import './main.css'
+import './plugins/material'
+import './plugins/toasted'
+import './plugins/mask'
+import './services/firebase'
+
+import Vue from 'vue'
 import App from './App'
 import store from './store'
 import router from './router'
-
-import './services/firebase'
-import './plugins/toasted'
-import './plugins/mask'
-import './plugins/material'
 
 Vue.config.productionTip = false
 
