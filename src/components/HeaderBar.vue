34bcca687957e470b4cf637c3446ae72e78fac89
Marlon Santos Constante <marlon.constante@gmail.com>
2017-10-15 18:05:52 -0200

    Componente HeaderBar

---
+++ src/components/HeaderBar.vue
@@ -0,0 +1,33 @@
+<template>
+    <md-layout class="headerBar">
+        <md-layout md-vertical-align="start">
+            <img src="../assets/logo.svg">
+        </md-layout>
+
+        <md-layout md-align="end">
+            <user-box name="Rony Hanksfield" community="Community Name" address="1855 Broadway (at 61st Street) New York, NY 10023" />
+        </md-layout>
+    </md-layout>
+</template>
+
+<script>
+import UserBox from '@/components/UserBox'
+
+export default {
+    name: 'HeaderBar',
+    components: {
+        UserBox
+    }
+}
+</script>
+
+<style scoped>
+.headerBar {
+    position: fixed;
+    width: 100%;
+}
+
+.headerBar img {
+    margin: 40px;
+}
+</style>
--- src/pages/Welcome.vue
+++ src/pages/Welcome.vue
@@ -1,14 +1,6 @@
 <template>
     <div class="full-height">
-        <md-layout class="header">
-            <md-layout md-vertical-align="start">
-                <img src="../assets/logo.svg">
-            </md-layout>
-
-            <md-layout md-align="end">
-                <user-box name="Rony Hanksfield" community="Community Name" address="1855 Broadway (at 61st Street) New York, NY 10023" />
-            </md-layout>
-        </md-layout>
+        <header-bar />
 
         <md-layout class="full-height" md-vertical-align="center" md-align="center">
             <info-box :title="title" :message="message" />
@@ -17,7 +9,7 @@
 </template>
 
 <script>
-import UserBox from '@/components/UserBox'
+import HeaderBar from '@/components/HeaderBar'
 import InfoBox from '@/components/InfoBox'
 
 export default {
@@ -29,19 +21,12 @@ export default {
         }
     },
     components: {
-        UserBox,
+        HeaderBar,
         InfoBox
     }
 }
 </script>
 
-<style scoped>
-.header {
-    position: fixed;
-    width: 100%;
-}
+<style>
 
-.header img {
-    margin: 40px;
-}
 </style>
