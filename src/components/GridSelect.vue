d52d233a100c8d55b70c89d93b56a6e2eb3602c3
Marlon Santos Constante <marlon.constante@gmail.com>
2017-10-18 09:59:37 -0200

    Componente GridSelect

---
+++ src/components/GridSelect.vue
@@ -0,0 +1,47 @@
+<template>
+    <md-layout class="gridSelect">
+        <div v-for="item in value">
+            <select-box :value="item" :fieldName="fieldName" @selectedValue="onSelectedValue" @unselectedValue="onUnselectedValue" />
+        </div>
+    </md-layout>
+</template>
+
+<script>
+import SelectBox from '@/components/SelectBox'
+
+export default {
+    name: 'GridSelect',
+    components: {
+        SelectBox
+    },
+    data() {
+        return {
+            selectedValues: []
+        }
+    },
+    props: {
+        fieldName: {
+            type: String
+        },
+        value: {
+            type: Array,
+            required: true
+        }
+    },
+    methods: {
+        onSelectedValue(value) {
+            this.selectedValues.push(value)
+        },
+        onUnselectedValue(value) {
+            this.selectedValues.pop(value)
+        }
+    }
+}
+</script>
+
+<style scoped>
+.gridSelect {
+    margin: 5px;
+    justify-content: center;
+}
+</style>
