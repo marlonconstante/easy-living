ae493fea815e55d733591f455396ef03e4d3d829
Marlon Santos Constante <marlon.constante@gmail.com>
2017-10-20 23:11:04 -0200

    Link com suporte ao teclado

--- src/components/LinkButton.vue
+++ src/components/LinkButton.vue
@@ -1,5 +1,5 @@
 <template>
-    <div class="linkButton" @click="onClick">
+    <div tabindex="0" class="linkButton" @keyup.enter="onClick" @keyup.space="onClick" @click="onClick">
         <md-ink-ripple />
         <slot />
     </div>
@@ -20,9 +20,15 @@ export default {
 .linkButton {
     cursor: pointer;
     position: relative;
+    outline: none;
     padding: 15px;
     font-size: 14px;
     font-weight: 700;
     color: #ffffff;
+}
+
+.linkButton:hover,
+.linkButton:focus {
+    background-color: rgba(255, 255, 255, 0.05);
 }
 </style>
