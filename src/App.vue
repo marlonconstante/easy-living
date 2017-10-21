ab3ef049553478d399167321e40ac0647bf35ff8
Marlon Santos Constante <marlon.constante@gmail.com>
2017-10-21 17:27:54 -0200

    Removendo implementação do evento “beforeCreate” do App

--- src/App.vue
+++ src/App.vue
@@ -7,23 +7,8 @@
 </template>
 
 <script>
-import Firebase from 'firebase'
-
 export default {
-    name: 'App',
-    beforeCreate() {
-        Firebase.auth().onAuthStateChanged((user) => {
-            if (user) {
-                const customerRef = Firebase.database().ref(`customers/${user.uid}`)
-                this.$bindAsObject('user', customerRef, null, () => {
-                    this.user.id = user.uid
-                    this.user.email = user.email
-
-                    console.log(this.user)
-                })
-            }
-        });
-    }
+    name: 'App'
 }
 </script>
 
