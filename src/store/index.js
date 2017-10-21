266cb0057f341e26a5acb84479a3c80140d71306
Marlon Santos Constante <marlon.constante@gmail.com>
2017-10-21 09:08:39 -0200

    vuex-loading

--- src/store/index.js
+++ src/store/index.js
@@ -1,7 +1,12 @@
 import Vue from 'vue'
 import Vuex from 'vuex'
+import { createVuexLoader } from 'vuex-loading'
+
+const VuexLoading = createVuexLoader()
 
 Vue.use(Vuex)
+Vue.use(VuexLoading)
 
 export default new Vuex.Store({
+    plugins: [VuexLoading.Store]
 })
