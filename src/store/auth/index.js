dfb518d3e9bdc186321f6a5a593b3fd306d9eebb
Marlon Santos Constante <marlon.constante@gmail.com>
2017-10-21 22:20:13 -0200

    Atribuindo um novo usuário em branco e ação para zerar o mesmo

--- src/store/auth/index.js
+++ src/store/auth/index.js
@@ -1,22 +1,33 @@
+const CLEAR_NEW_USER = 'CLEAR_NEW_USER'
 const SET_NEW_USER = 'SET_NEW_USER'
 const REGISTER_NEW_USER = 'REGISTER_NEW_USER'
 const LOGIN_USER = 'LOGIN_USER'
 const LOGOUT_USER = 'LOGOUT_USER'
 
+const emptyUser = {
+    name: '',
+    birthDate: '',
+    address: '',
+    community: ''
+}
+
 export default {
     state: {
-        newUser: null,
+        newUser: emptyUser,
         currentUser: null
     },
     getters: {
         isNewAccount: state => {
-            return state.newUser != null
+            return state.newUser != emptyUser
         },
         isLoggedUser: state => {
             return state.currentUser != null
         }
     },
     mutations: {
+        [CLEAR_NEW_USER](state) {
+            state.newUser = emptyUser
+        },
         [SET_NEW_USER](state, payload) {
             state.newUser = payload
         },
@@ -31,6 +42,9 @@ export default {
         }
     },
     actions: {
+        clearNewUser({ commit }) {
+            commit(CLEAR_NEW_USER)
+        },
         setNewUser({ commit }, user) {
             commit(SET_NEW_USER, user)
         }
