0a490c12af3ffcf586983014c0e52981e78066a4
Marlon Santos Constante <marlon.constante@gmail.com>
2017-10-22 21:36:03 -0200

    Ação para carregar o usuário logado do Firebase

--- src/store/auth/index.js
+++ src/store/auth/index.js
@@ -62,6 +62,18 @@ export default {
             const user = await auth.signInWithEmailAndPassword(email, password)
             commit(SET_CURRENT_USER, await getUserData(user))
         },
+        loadUser({ commit }) {
+            return new Promise((resolve, reject) => {
+                auth.onAuthStateChanged((user) => {
+                    if (user) {
+                        commit(SET_CURRENT_USER, await getUserData(user))
+                    } else {
+                        commit(SET_CURRENT_USER, null)
+                    }
+                    resolve()
+                })
+            })
+        },
         async logoutUser({ commit }) {
             await auth.signOut()
             commit(SET_CURRENT_USER, null)
