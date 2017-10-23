27b1790aadce12957d4918940e81ddd7c63b108c
Marlon Santos Constante <marlon.constante@gmail.com>
2017-10-23 10:49:58 -0200

    Transição de telas com efeito de fade

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,9 @@
 <template>
     <div id="app">
         <div class="content">
-            <router-view></router-view>
+            <transition name="fade">
+                <router-view></router-view>
+            </transition>
         </div>
     </div>
 </template>
