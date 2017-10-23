645586383d6eea567157c1d0fe0d6633eeff201b
Marlon Santos Constante <marlon.constante@gmail.com>
2017-10-23 10:21:17 -0200

    Tela de boas-vindas funcional

--- src/pages/Welcome.vue
+++ src/pages/Welcome.vue
@@ -1,5 +1,5 @@
 <template>
-    <div class="full-height">
+    <div class="welcome full-height">
         <header-bar />
 
         <md-layout class="full-height" md-vertical-align="center" md-align="center">
@@ -9,24 +9,38 @@
 </template>
 
 <script>
+import { mapGetters, mapState } from 'vuex'
 import HeaderBar from '@/components/HeaderBar'
 import InfoBox from '@/components/InfoBox'
 
 export default {
     name: 'Welcome',
-    data() {
-        return {
-            title: 'Welcome, <b>Rony</b>!',
-            message: 'We are here to help you.'
-        }
-    },
     components: {
         HeaderBar,
         InfoBox
+    },
+    computed: {
+        ...mapGetters('auth', ['isLoggedUser']),
+        ...mapState('auth', ['newUser', 'currentUser']),
+        greeting() {
+            return this.isLoggedUser ? 'Hello' : 'Welcome'
+        },
+        firstUserName() {
+            return (this.currentUser || this.newUser).name.split(' ')[0].toLowerCase()
+        },
+        title() {
+            return `${this.greeting}, <span class="userName">${this.firstUserName}</span>!`
+        },
+        message() {
+            return this.isLoggedUser ? 'It\'s good to see you again.' : 'We are here to help you.'
+        }
     }
 }
 </script>
 
 <style>
-
+.welcome .userName {
+    font-weight: 700;
+    text-transform: capitalize;
+}
 </style>
