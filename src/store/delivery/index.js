3c877863c78f99328415ff768bfe8a993026e431
Marlon Santos Constante <marlon.constante@gmail.com>
2017-10-23 15:21:19 -0200

    Incluindo lista de todas as lojas na store

--- src/store/delivery/index.js
+++ src/store/delivery/index.js
@@ -1,23 +1,47 @@
+import Firebase from 'firebase'
+
 const SET_STORES = 'SET_STORES'
+const SET_SELECTED_STORES = 'SET_SELECTED_STORES'
 const SET_INSTRUCTION = 'SET_INSTRUCTION'
+
+const db = Firebase.database()
 
 export default {
     namespaced: true,
     state: {
         stores: [],
+        selectedStores: [],
         instruction: ''
+    },
+    getters: {
+        isLoadedStores: state => {
+            return state.stores.length > 0
+        }
     },
     mutations: {
         [SET_STORES](state, payload) {
             state.stores = payload
+        },
+        [SET_SELECTED_STORES](state, payload) {
+            state.selectedStores = payload
         },
         [SET_INSTRUCTION](state, payload) {
             state.instruction = payload
         }
     },
     actions: {
-        setStores({ commit }, stores) {
+        async loadStores({ commit }) {
+            const stores = []
+
+            const snapshot = await db.ref('stores').once('value')
+            snapshot.forEach(child => {
+                stores.push(child.val())
+            });
+
             commit(SET_STORES, stores)
+        },
+        setSelectedStores({ commit }, stores) {
+            commit(SET_SELECTED_STORES, stores)
         },
         setInstruction({ commit }, instruction) {
             commit(SET_INSTRUCTION, instruction)
